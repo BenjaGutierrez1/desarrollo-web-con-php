3cca5502f2d27a6df0a953020c5c591162bdd133
benjamin gutierrez <benjamin.gutierrez4@mail.udp.cl>
2026-05-14 10:17:11 -0400

    prueba de rama

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Programacion Web</title>
+    <title>Programacion Web en PHP</title>
 
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
